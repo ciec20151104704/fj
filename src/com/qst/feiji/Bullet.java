8e7754842084102be70360011079b9430522a995
ciec20151104704 <s20151104704@chaolimengdeiMac.local>
2018-07-05 19:56:19 +0800

    s

---
+++ src/com/qst/feiji/Bullet.java
@@ -0,0 +1,22 @@
+package com.qst.feiji;
+
+public class Bullet extends FlyingObject {
+
+	public Bullet (int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public boolean outOfBounds() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/qst/feiji/ShootGame.java
@@ -0,0 +1,11 @@
+package com.qst.feiji;
+
+import java.awt.image.BufferedImage;
+
+public class ShootGame {
+
+	public static BufferedImage hero0;
+	public static BufferedImage hero1;
+	
+
+}
